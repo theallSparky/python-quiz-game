349d31ebaabcabde2d758f3f1eca6c78707c6f5d
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-25 07:58:48 -0500

    Started logic on quiz_game.py

---
+++ quiz_game.py
@@ -0,0 +1,58 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay, let's play!")
+
+answer1 = input("What does CPU stand for? ")
+if answer1 == "central processing unit":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer2 = input("What does GPU stand for? ")
+if answer2 == "graphics processing unit":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer3 = input("What year did the great war begin in Fallout? ")
+if answer3 == "2076":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer4 = input("What does RAM stand for? ")
+if answer4 == "random access memory":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer5 = input(" ")
+if answer5 == "":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer6 = input(" ")
+if answer6 == "":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer7 = input(" ")
+if answer7 == "":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer8 = input(" ")
+if answer8 == "":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer9 = input(" ")
+if answer9 == "":
+    print("Correct!")
+else: print("Incorrect!")
+
+answer10 = input(" ")
+if answer10 == "":
+    print("Correct!")
+else: print("Incorrect!")
