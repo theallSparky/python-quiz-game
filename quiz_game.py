2aa1804d8c113bddb86ab2076f7dc54f8315c907
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-03-25 08:37:11 -0500

    Added .lower() to all if statements so answer may be accepted regardless of capitals or not

--- quiz_game.py
+++ quiz_game.py
@@ -2,21 +2,21 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
-print("Okay, let's play! Please input all answers as lowercase, even for proper nouns.")
+print("Okay, let's play!")
 
 score = 0
 
 answer1 = input("What does CPU stand for? ")
-if answer1 == "central processing unit":
+if answer1.lower() == "central processing unit":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
 
 answer2 = input("What does GPU stand for? ")
-if answer2 == "graphics processing unit":
+if answer2.lower() == "graphics processing unit":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
@@ -28,37 +28,37 @@ if answer3 == "2076":
 else: print("Incorrect!")
 
 answer4 = input("What does RAM stand for? ")
-if answer4 == "random access memory":
+if answer4.lower() == "random access memory":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
 
 answer5 = input("Who is the main antagonist in Borderlands 2? ")
-if answer5 == "handsome jack":
+if answer5.lower() == "handsome jack":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
 
 answer6 = input("What is the scientific division which both german scientists Edward Richtofen and Ludvig Maxis joined? group 88, division 66, group 935, or alliance 725? ")
-if answer6 == "group 935":
+if answer6.lower() == "group 935":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
 
 answer7 = input("In Der Eisendrache, which of the 4 characters are we there for? Richtofen, Takeo, Dempsey, or Nikolai? ")
-if answer7 == "dempsey":
+if answer7.lower() == "dempsey":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
 
 answer8 = input("What was the original name of the Russian city Stalingrad? ")
-if answer8 == "leningrad":
+if answer8.lower() == "leningrad":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
 
 answer9 = input("Who is the current leader of the country of Russia? (last name only, also lowercase!) ")
-if answer9 == "putin":
+if answer9.lower() == "putin":
     print("Correct!")
     score+=1
 else: print("Incorrect!")
